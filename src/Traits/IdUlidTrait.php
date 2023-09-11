a0d687fe3e1a27f89527b5b7525d1ef7089df873
Axel Barlet <axelbarlet16@gmail.com>
2023-09-11 20:48:40 +0200

    feat: Add UUIDTrait and ULIDTrait

---
+++ src/Traits/IdUlidTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Barlito\Utils\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\IdGenerator\UlidGenerator;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+trait IdUlidTrait
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="ulid", unique=true, name="id")
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class=UlidGenerator::class)
+     * @Assert\Ulid()
+     */
+    #[Groups(['default'])]
+    private string $id;
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+}
---
+++ src/Traits/IdUuidTrait.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Barlito\Utils\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\IdGenerator\UuidGenerator;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+trait IdUuidTrait
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="guid", unique=true, name="id")
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class=UuidGenerator::class)
+     * @Assert\Uuid()
+     */
+    #[Groups(['default'])]
+    private string $id;
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+}
